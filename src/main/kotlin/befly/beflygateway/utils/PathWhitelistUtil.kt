bad92b2d4c626d3fc6dda42a4e63f056ab170cc6
gohyeoncheol <huncher00@naver.com>
2025-06-06 17:18:22 +0900

    :bug: fix: [KAN-82] 화이트리스트 수정

--- src/main/kotlin/befly/beflygateway/utils/PathWhitelistUtil.kt
+++ src/main/kotlin/befly/beflygateway/utils/PathWhitelistUtil.kt
@@ -4,19 +4,19 @@ import org.springframework.util.AntPathMatcher
 
 object PathWhitelistUtil {
     private val matcher = AntPathMatcher()
-    private val whiteListPatterns = listOf(
-        "/oauth2/**",
-        "/login/**",
-        "/auth/**",
-        "/swagger-ui/**",
-        "/v3/api-docs/**",
-        "/api/docs",
-        "/api/**",
-        "/favicon.ico",
-        "/asdfasdf",
+    private val whitelistRules: List<Pair<String, String>> = listOf(
+            "ANY" to "/auth/**",
+            "ANY" to "/login/**",
+            "ANY" to "/swagger-ui/**",
+            "ANY" to "/v3/api-docs/**",
+            "ANY" to "/favicon.ico", // 모든 method 허용할 경우
+            "GET" to "/community/**",
     )
 
-    fun isWhitelisted(path: String): Boolean {
-        return whiteListPatterns.any { pattern -> matcher.match(pattern, path) }
+    fun isWhitelisted(method: String, path: String): Boolean {
+        return whitelistRules.any { (ruleMethod, pattern) ->
+            (ruleMethod == "ANY" || ruleMethod.equals(method, ignoreCase = true)) &&
+                    matcher.match(pattern, path)
+        }
     }
 }
